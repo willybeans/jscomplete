26d75f1388b46fb3571ac52f4c0416e8387acd28
William Wedmedyk <williamwedmedyk@William-Wedmedyks-MacBook.local>
2017-01-13 12:34:45 -0500

    second commit

---
+++ dif-server-files-from-video/server.js
@@ -0,0 +1,5 @@
+import config, { nodeEnv, logStars} from './config';
+
+logStars('Function');
+
+console.log(config, nodeEnv);
---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+class App extends React.Component {
+	state = {	
+		pageHeader:  'Naming Contests'
+		};
+	render() {
+	return (
+	<div className="App">
+		 <Header message={this.state.pageHeader}/>
+		<div>
+			{this.props.contests.map(contest =>
+			 <ContestPreview {...contest}/>
+		)}
+		</div>
+	</div>
+	);
+}
+}
+
+export default App;
